0f0979a4ff269de2cab2e531219cc6e9c6945f46
Sophie <gnitedemsophiemanuella@gmail.com>
2024-09-06 12:33:08 +0200

    creating the change paswword

--- src/pages/auth/login/buttons/ChangePassword.js
+++ src/pages/auth/login/buttons/ChangePassword.js
@@ -5,12 +5,56 @@ import Loader from '../../../../components/loader/Loader'
 
 
 const ChangePassword = () => {
-    const [password, setPassword] = useState('')
+    const [newPassword, setNewPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [error, setError] = useState('')
     const [message, setMessage] = useState('')
+    const [success, setSuccess] = useState('')
+    
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
 
-    const handle_request_otp = async(e) => {
+    const handle_submit = async (e) => {
+        e.preventDefault();
+        setError('');
+        setSuccess('');
+
+        if (!newPassword) {
+            setError('Please enter a new password.');
+            return;
+        }
+
+        if (newPassword !== confirmPassword){
+            setError('Passwords do not match.');
+            return;
+        }
+
+       setLoading(true);
+
+       const user = JSON.parse(localStorage.getItem('user'));
+       const uid = user?.uid; 
+       const token = user?.token; 
+       
+        
+        const response = await auth('/auth/reset-password', { uid,
+            token,
+           
+            new_password: newPassword });
+            console.log(response);
+  
+        if (response.status === 200) {
+          setSuccess('Password changed successfully');
+          navigate('/auth/password-reset-sent'); 
+        } else {
+          setError('Failed to change password');
+        }
+      
+       setLoading(false);
+      
+    };
+  
+
+    /*const handle_request_otp = async(e) => {
         e.preventDefault()
         setLoading(true)
         const response = await auth('/auth/verify/otp/', { password })
@@ -23,13 +67,13 @@ const ChangePassword = () => {
         }
         
         setLoading(false)
-    }
+    }*/
   return (
     <div className='auth'>
         
         <div className='container'>
             <div className='auth_card_wrapper'>
-                    <form onSubmit={ handle_request_otp } className='main_heading'>
+                    <form onSubmit={ handle_submit } className='main_heading'>
                         <div className='auth_heading_thing'>
                             <h1>Create new Password</h1>
                             <p>Once you confirm your new password, your password</p>
@@ -43,11 +87,15 @@ const ChangePassword = () => {
                         
 
                         <div className='auth_form_input'>
-                            <input type='text' placeholder='New password' onChange={e => setPassword(e.target.value)} value={password} />
+                            <input type='password' placeholder='New password' onChange={(e) => setNewPassword(e.target.value)} value={newPassword} />
                         </div>
                         <div className='auth_form_input'>
-                            <input type='text' placeholder='Confirm new password' onChange={e => setPassword(e.target.value)} value={password} />
+                            <input type='password' placeholder='Confirm new password' onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} />
+
                         </div>
+                        {error && <p style={{ color: 'red'}}>{error}</p>}
+                        {success && <p style={{ color: 'gren'}}>{success}</p>}
+                        
                         {
                             loading
                             ?
@@ -57,7 +105,10 @@ const ChangePassword = () => {
                             :
                             <div className='auth_form_input btn'>
                                 <input type='submit' value='Change Password' />
+                                
                             </div>
+                            
+                           
                         }
                         
                     </form>
