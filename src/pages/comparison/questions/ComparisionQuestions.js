4f0eb87477821dca087333d470d94425cdfefb71
kimbi619 <kimbidarl@gmail.com>
2024-09-21 12:46:35 -0400

    new ui

--- src/pages/comparison/questions/ComparisionQuestions.js
+++ src/pages/comparison/questions/ComparisionQuestions.js
@@ -11,6 +11,10 @@ import { VscClose, VscArrowRight, VscArrowLeft } from 'react-icons/vsc';
 import UserForm from './components/UserForm';
 import { tabTitle } from '../../..';
 import i18next from 'i18next';
+import SearchableList from './components/SearchableList';
+import OptionButtons from './components/OptionButtons';
+import ModalSelect from './components/ModalSelect';
+import VehicleYearSelector from './components/VehicleYearSelector';
 
 const API_MANAGER = [
   { insurance_type: 'life', estimated_questions: 22, base_url: '/life-insurance/comparison/stage/', complete_url: "/life-insurance/comparison/complete/", result_page: "/comparison/result/life" },
@@ -244,6 +248,10 @@ const InsuranceQuestions = () => {
                 <h2>{currentQuestion.next_stage}</h2>
                 <p>{ lang === 'en' ? currentQuestion.question.en :  currentQuestion.question.fr}</p>
                 {
+                  currentQuestion?.api && currentQuestion?.type === 'multiple_select' 
+                  ? 
+                    <APIMultipleSelect api={currentQuestion?.api} /> 
+                  :
                   currentQuestion?.api 
                   ? 
                     <APISelect api={currentQuestion?.api} /> 
@@ -284,29 +292,30 @@ const APISelect = ({ api }) => {
   const [list, setlist] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
-  const [selectedOption, setSelectedOption] = useState(null)
   const [checkedItems, setCheckedItems] = useState({});
 
   
   const context = useQuestionContext();
-  const data = {
-    "vehicle_make": "1"
-  }
+  
   useEffect(() => {
     fetch_data()
   }, [])
 
+  /**
+   * this function replaces the variables in the url with the current answer
+   * @param { String } url the url to be replaced
+   * @returns a new url with the variables replaced
+   */
   const replaceUrlVariables = (url) => {
-    // return url.replace(/\{(\w+)\}/g, (match, variable) => {
-    //   return data.hasOwnProperty(variable) ? data[variable] : match;
-    // });
     return url.includes("{mark_id}") ? url.replace(/\{(\w+)\}/g, currentAnswer) : url
   };
 
+
+  /**
+   * Fetch data from the API
+   */
   const fetch_data = async () => {
     const url = replaceUrlVariables(api)
-    console.log('makeing request url', url);
-    console.log('That current question', currentAnswer);
     
     try {
       setLoading(true)
@@ -315,7 +324,7 @@ const APISelect = ({ api }) => {
     }
     catch (err) {
       setError('An error occurred while fetching the data.');
-      console.error(err);
+      console.warn(err);
     } finally {
       setLoading(false);
     }
@@ -343,15 +352,16 @@ const APISelect = ({ api }) => {
 
   return (
     <div className='options'>
-
+    {/* 
       <select onClick={(e) => handleAnswer(e.target.value)}>
         {
           list.map(listItem => (
             <option key={listItem.value} value={listItem.value}>{listItem.value}</option> 
           ))
         }
-      </select>
+      </select> */}
 
+      <SearchableList list={list} onSelect={handleAnswer} />
     </div>
   )
 }
@@ -439,6 +449,7 @@ const APIMultipleSelect = ({ api }) => {
     </div>
   )
 }
+
 const MultipleSelect = ({ choices }) => {
   const [checkedItems, setCheckedItems] = useState({});
 
@@ -460,7 +471,9 @@ const MultipleSelect = ({ choices }) => {
     handleAnswer(JSON.stringify(data));
   };
 
-
+  if(currentQuestion.api) {
+    return 
+  }
   return (
     <div className='options'>
       <div class="comparison_select_multiple_options">
@@ -484,149 +497,6 @@ const MultipleSelect = ({ choices }) => {
 }
 
 
-const ModalSelect = ({ api, currentQuestion, handleAnswer }) => {
-  const [list, setlist] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-  const [selectedOption, setSelectedOption] = useState(null)
-  const [contentList, setContentList] = useState(null)
-
-  useEffect(() => {
-    fetch_data()
-  }, [])
-
-  const fetch_data = async () => {
-    try {
-      setLoading(true)
-      const response = await axios.get(api)
-      response.status === 200 && setlist(response.data?.results)
-      const selected = response.data?.results.find(item => item.type === selectedOption)
-      setContentList(selected)
-      console.log(response.data);
-    }
-    catch (err) {
-      setError('An error occurred while fetching the data.');
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  }
-  const buttons = [
-    "Basic Insurance",
-    "Standard Insurance",
-    "Premium Insurance"
-  ]
-
-  const handle_remove_modal = () => {
-    setSelectedOption(null)
-  }
-
-  const update_selected_option = (option) => {
-    console.log(option)
-    setSelectedOption(option)
-    const selected = list.find(item => item.type === option)
-    setContentList(selected)
-  }
-
-  const confirm_option = () => {
-    handleAnswer(selectedOption)
-    handle_remove_modal()
-  }
- 
-  return (
-    <>
-      <div className="options">
-        {Array.isArray(currentQuestion.choices) ? currentQuestion.choices.map(choice => (
-          <button
-            key={choice.code}
-            onClick={() => update_selected_option(choice.code)}
-          >
-            {choice.en}
-          </button>
-        )) : <div>No options available</div>}
-      </div>
-
-        {
-          selectedOption && 
-          <>
-          <div className='insurance_modal_select'>
-            <div className='modal_select'>
-              { loading && <div className='Loader' />}
-              <h2>Coverage details for your selected Insurance</h2>
-              <div className='insurance_modal_buttons_wrapper'>
-                {
-                  list.map((listItem) => (
-                    <button className={selectedOption === listItem?.type ? "selected": ""} key={listItem?.id} onClick={() => update_selected_option(listItem?.type)}>{listItem?.type} Insurance</button>
-                  ))
-                }
-              </div>
-              <div className='insurance_modal_select_contents'>
-                <li className="insurance_list_partial_results">
-                  <FaCheckCircle className="text-green-500 mr-2" />
-                  <span className="font-medium">Supported Hospitals: </span>
-                  <span className="ml-2">{ contentList?.public_hospitals && 'Public Hospitals' } { contentList?.para_public_hospitals && '- Para-public Hospitals ' } { contentList?.private_hospitals && '- Private Hospitals' }</span>
-                </li>
-                <li className="insurance_list_partial_results">
-                  <FaCheckCircle className="text-green-500 mr-2" />
-                  <span className="font-medium">Coverage Rate: </span>
-                  <span className="ml-2">{ contentList?.coverage_rate }% </span>
-                </li>
-                <li className="insurance_list_partial_results">
-                  { contentList?.consultation_general_practitioner ? <FaCheckCircle className="text-green-500 mr-2" /> : <FaTimesCircle className="text-green-500 mr-2" style={{ color: 'var(--red)' }} /> }
-                  <span className="font-medium">General Consultation: </span>
-                  <span className="ml-2" style={{ color: contentList?.consultation_general_practitioner ? 'var(--green)' :  'var(--red)' }}>{ contentList?.consultation_general_practitioner ? 'Avalaible' : 'Unavailable' }</span>
-                </li>
-                <li className="insurance_list_partial_results">
-                  { contentList?.consultation_specialist ? <FaCheckCircle className="text-green-500 mr-2" /> : <FaTimesCircle className="text-green-500 mr-2" style={{ color: 'var(--red)' }} /> }
-                  <span className="font-medium">Special Consultation: </span>
-                  <span className="ml-2" style={{ color: contentList?.consultation_specialist ? 'var(--green)' :  'var(--red)' }}>{ contentList?.consultation_specialist ? 'Avalaible' : 'Unavailable' }  </span>
-                </li>
-                <li className="insurance_list_partial_results">
-                  { contentList?.consultation_professor ? <FaCheckCircle className="text-green-500 mr-2" /> : <FaTimesCircle className="text-green-500 mr-2" style={{ color: 'var(--red)' }} /> }
-                  <span className="font-medium">Expert Consultation: </span>
-                  <span className="ml-2" style={{ color: contentList?.consultation_professor ? 'var(--green)' :  'var(--red)' }}>{ contentList?.consultation_professor ? 'Avalaible' : 'Unavailable' }  </span>
-                </li>
-                <li className="insurance_list_partial_results">
-                  { contentList?.hospitalization ? <FaCheckCircle className="text-green-500 mr-2" /> : <FaTimesCircle className="text-green-500 mr-2" style={{ color: 'var(--red)' }} /> }
-                  <span className="font-medium">Hospitalization: </span>
-                  <span className="ml-2"  style={{ color: contentList?.hospitalization ? 'var(--green)' :  'var(--red)' }}>{ contentList?.hospitalization  ? 'Avalaible' : 'Unavailable' } </span>
-                </li>
-                <li className="insurance_list_partial_results">
-                  { contentList?.nuclear_medicine ? <FaCheckCircle className="text-green-500 mr-2" /> : <FaTimesCircle className="text-green-500 mr-2" style={{ color: 'var(--red)' }} /> }
-                  <span className="font-medium">Nuclear Medicine: </span>
-                  <span className="ml-2" style={{ color: contentList?.nuclear_medicine ? 'var(--green)' :  'var(--red)' }}>{ contentList?.nuclear_medicine ? 'Avalaible' : 'Unavailable' } </span>
-                </li>
-                <li className="insurance_list_partial_results">
-                  { contentList?.optics_every_two_years ? <FaCheckCircle className="text-green-500 mr-2" /> : <FaTimesCircle className="text-green-500 mr-2" style={{ color: 'var(--red)' }} /> }
-                  <span className="font-medium">Optics once/2years: </span>
-                  <span className="ml-2" style={{ color: contentList?.optics_every_two_years ? 'var(--green)' :  'var(--red)' }}>{ contentList?.optics_every_two_years ? 'Avalaible' : 'Unavailable' } </span>
-                </li>
-                <li className="insurance_list_partial_results">
-                  { contentList?.speech_therapy ? <FaCheckCircle className="text-green-500 mr-2" /> : <FaTimesCircle className="text-green-500 mr-2" style={{ color: 'var(--red)' }} /> }
-                  <span className="font-medium">Speech Therapy: </span>
-                  <span className="ml-2" style={{ color: contentList?.speech_therapy ? 'var(--green)' :  'var(--red)' }}>{ contentList?.speech_therapy ? 'Avalaible' : 'Unavailable' } </span>
-                </li>
-
-
-
-
-              </div>
-
-              <div className='insurance_list_selected_btn' onClick={confirm_option}>Select</div>
-
-              <div onClick={handle_remove_modal} className='logout_modal_close'>
-                <VscClose />
-              </div>
-
-            </div>
-            
-
-          </div>
-          </>
-        }
-    </>
-  )
-}
 
 
 
@@ -649,20 +519,12 @@ const QuestionOptions = () => {
     case 'multiple_choice':
       return (
         <div className="options">
-          {Array.isArray(currentQuestion.choices) ? currentQuestion.choices.map(choice => (
-            <button
-              key={choice.code}
-              onClick={() => handleAnswer(choice.code)}
-              className={currentAnswer === choice.code ? 'selected' : ''}
-            >
-              { lang === 'en' ? choice.en : choice.fr }
-            </button>
-          )) : <div>No options available</div>}
+          <OptionButtons options={currentQuestion.choices} handleAnswer={handleAnswer} currentAnswer={currentAnswer} lang={lang} />
         </div>
       );
     case "multiple_choice_with_icon":
       return (
-        <div className="options">
+        <div className="options options_with_icon">
           {Array.isArray(currentQuestion.choices) ? currentQuestion.choices.map(choice => (
             <div
               key={choice.code}
@@ -717,15 +579,19 @@ const QuestionOptions = () => {
       return (
        <MultipleSelect choices={currentQuestion.choices} />
       );
+    // case 'date':
+    //   return (
+    //     <div className="options">
+    //       <input
+    //         type="date"
+    //         value={currentAnswer || ''}
+    //         onChange={(e) => handleAnswer(e.target.value)}
+    //       />
+    //     </div>
+    //   );
     case 'date':
       return (
-        <div className="options">
-          <input
-            type="date"
-            value={currentAnswer || ''}
-            onChange={(e) => handleAnswer(e.target.value)}
-          />
-        </div>
+        <VehicleYearSelector onYearSelect={handleAnswer} />
       );
     case 'user_form_field':
       return (
@@ -750,7 +616,6 @@ const SVGIcon = ({ svgString }) => {
     return <span dangerouslySetInnerHTML={{ __html: svgElement.outerHTML }} />;
   } 
   else {
-    console.error('Failed to parse SVG:', svgString);
     return <span>Icon</span>;
   }
 }
