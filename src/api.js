30f9392ff45b427f084fd0c2e4dc9462f09e70f6
FRITZBAY <57754285+FRITZBAY@users.noreply.github.com>
2025-04-28 14:59:47 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,313 +1,63 @@
 import axios from "axios";
 
+// Récupération de l'utilisateur et de son token
+const user = JSON.parse(localStorage.getItem('user'));
+const token = user?.access || "";
 
-const user = JSON.parse(localStorage.getItem('user'))
+// URL de base pour les requêtes API
+const BASEURL = "https://harpie-app.site/api/v1";
 
-const token = user?.access ? user.access : ""
-
-const configToken = {
-    headers: {
+// Fonction pour générer la configuration des en-têtes
+const getConfig = (sessionId = null) => {
+    const headers = {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-    }
-}
-
-const config = {
-    headers: {
-        "Content-Type": "application/json",
-    }
-}
-
-const configGlobal = token ? configToken : config
-
-const BASEURL = "https://harpie-app.site/api/v1"
-
-
-
-/**
- * Function to make a GET Request
- * @param { String } endpoint The endpoint to make the request to 
- * @returns A promise that resolves to the response of the request
- */
-export const getRequest = async(endpoint) => {
-    try {
-        const response = await axios.get(`${BASEURL}${endpoint}`)
-        return response
-    } catch (err) {
-        return err
-    }
-}
-
-
-/**
- * Function to make a DELETE Request
- * @param { String } endpoint It is the endpoint to make the request to
- * @returns Makes a POST Request
- */
-export const deleteRequest = async(endpoint) => {
-    try {
-        const response = await axios.delete(`${BASEURL}${endpoint}`, configGlobal);
-        return response
-    } catch (error) {
-        console.error(error);
-        return error
-    }
-}
-
-
-/**
- * Function to make a PATCH/PARTIAL UPDATE Request
- * @param { String } endpoint The endpoint to make the request to
- * @param { Object } data The data to send in the request
- * @returns A promise that resolves to the response of the request
- */
-export const patchRequest = async(endpoint, data) => {
-    try {
-        const response = await axios.patch(`${BASEURL}${endpoint}`, data, configGlobal);
-        return response
-    } catch (error) {
-        console.error(error);
-        return error
-    }
-}
-
-
-/**
- * A function that makes API request to any external service used in the application
- * @param {String } endpoint Complete URL user wants to make a request to
- * @returns A promise that resolves to the response of the request
- */
-export const getExternalRequest = async(endpoint) => {
-    try {
-        const response = await axios.get(endpoint)
-        return response
-    } catch (err) {
-        return err
-    }
-}
-
-/**
- * Function to make a POST Request for general endpoints
- * @param { String } endpoint The endpoint to make the request to
- * @param { Object } data The data to send in the request
- * @returns A promise that resolves to the response of the request
- */
-export const  postRequest = async (endpoint, data) => {
-    const config = {
-        headers: {
-            "Content-Type": "application/json",
-        }
-    }
-    try {
-        const response = await axios.post(`${BASEURL}${endpoint}`, data, config);
-        
-        return response
-    } catch (error) {
-        console.error(error);
-        return error
-    }
-}
-/**
- * Function to make a POST Request for general endpoints
- * @param { String } endpoint The endpoint to make the request to
- * @param { Object } data The data to send in the request
- * @returns A promise that resolves to the response of the request
- */
-export const  postRequestWithLanguage = async (endpoint, data) => {
-    const config = {
-        headers: {
-            "Content-Type": "application/json",
-            "Accept-Language": "en"
-        }
-    }
-    try {
-        const response = await axios.post(`${BASEURL}${endpoint}`, data, config);
-        
-        return response
-    } catch (error) {
-        console.error(error);
-        return error
-    }
-}
-
-
-export const  getRequestWithLanguage = async (endpoint) => {
-    const config = {
-        headers: {
-            "Content-Type": "application/json",
-            "Accept-Language": "en"
-        }
-    }
-    try {
-        const response = await axios.get(`${BASEURL}${endpoint}`, config);
-        
-        return response
-    } catch (error) {
-        console.error(error);
-        return error
-    }
-}
-
-
-/**
- * Function to make a POST Request ( Authentication Specific Requests )
- * @param { String } endpoint The endpoint to make the request to
- * @param { Object } data The data to send in the request
- * @returns A promise that resolves to the response of the request
- */
-export const  auth = async (endpoint, data) => {
-    const config = {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }
-    try {
-        const response = await axios.post(`${BASEURL}${endpoint}`, data, config);
-        
-        return response
-    } catch (error) {
-        console.error(error);
-        return error
-    }
-}
-
-
-
-// ===================
-
-
-/**
- * Function to make a GET Request with session ID
- * @param { String } sessionId The session ID to include in the request
- * @param { String } endpoint The endpoint to make the request to 
- * @returns A promise that resolves to the response of the request
- */
-export const getRequestWithSession = async (sessionId, endpoint) => {
-    const configWithSession = {
-        headers: {
-            "Content-Type": "application/json",
-            "X-Session-ID": sessionId
-        }
     };
-    try {     
-        const response = await axios.get(`${BASEURL}${endpoint}`, configWithSession);
+    if (token) {
+        headers["Authorization"] = `Bearer ${token}`;
+    }
+    if (sessionId) {
+        headers["X-Session-ID"] = sessionId;
+    }
+    return { headers };
+};
+
+// Fonction générale pour faire des requêtes
+const apiRequest = async (method, endpoint, data = null, sessionId = null) => {
+    try {
+        const config = getConfig(sessionId);
+        const response = await axios({
+            method,
+            url: `${BASEURL}${endpoint}`,
+            data,
+            ...config,
+        });
         return response;
-    } catch (err) {
-        console.error("GET request error:", err);
-        return err;
+    } catch (error) {
+        console.error(`${method.toUpperCase()} request error:`, error);
+        throw error; // Rejeter l'erreur pour un traitement ultérieur
     }
 };
 
-/**
- * Function to make a POST Request with session ID
- * @param { String } sessionId The session ID to include in the request
- * @param { String } endpoint The endpoint to make the request to
- * @param { Object } data The data to send in the request
- * @returns A promise that resolves to the response of the request
- */
-export const postRequestWithSession = async (sessionId, endpoint, data) => {
-    const configWithSession = {
-        headers: {
-            "Content-Type": "application/json",
-            "X-Session-ID": sessionId,
-            "Authorization": `Bearer ${token}`
-        }
-    };
-    
+// Fonctions spécifiques pour les requêtes API
+export const getRequest = (endpoint) => apiRequest('get', endpoint);
+export const postRequest = (endpoint, data) => apiRequest('post', endpoint, data);
+export const deleteRequest = (endpoint) => apiRequest('delete', endpoint);
+export const patchRequest = (endpoint, data) => apiRequest('patch', endpoint, data);
+export const getRequestWithSession = (sessionId, endpoint) => apiRequest('get', endpoint, null, sessionId);
+export const postRequestWithSession = (sessionId, endpoint, data) => apiRequest('post', endpoint, data, sessionId);
+
+// Fonction pour faire un appel vers un service externe
+export const getExternalRequest = async (endpoint) => {
     try {
-        const response = await axios.post(`${BASEURL}${endpoint}`, data, configWithSession);
+        const response = await axios.get(endpoint);
         return response;
     } catch (error) {
-        console.error("POST request error:", error);
-        return error;
+        console.error("External request error:", error);
+        throw error;
     }
 };
 
-
-/**
- * Function to make a POST Request with session ID
- * @param { String } sessionId The session ID to include in the request
- * @param { String } endpoint The endpoint to make the request to
- * @param { Object } data The data to send in the request
- * @returns A promise that resolves to the response of the request
- */
-export const postRequestWithSessionNoAuth = async (sessionId, endpoint, data) => {
-    const configWithSession = {
-        headers: {
-            "Content-Type": "application/json",
-            "X-Session-ID": sessionId,
-        }
-    };
-    
-    try {
-        const response = await axios.post(`${BASEURL}${endpoint}`, data, configWithSession);
-        return response;
-    } catch (error) {
-        console.error("POST request error:", error);
-        return error;
-    }
-};
-
-
-export const authenticatedGetRequest = async (endpoint) => {
-    const config = {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
-        }
-    };
-
-    console.log('config', config)
-    
-    try {
-        const response = await axios.get(`${BASEURL}${endpoint}`, config);
-        return response;
-    } catch (error) {
-        console.error("POST request error:", error);
-        return error;
-    }
-};
-
-
-/**
- * Function to make a POST Request with session ID for authenticated users
- * @param { String } sessionId The session ID to include in the request
- * @param { String } endpoint The endpoint to make the request to
- * @param { Object } data The data to send in the request
- * @returns A promise that resolves to the response of the request
- */
-export const authenticatedPostRequestWithSession = async (sessionId, endpoint, data) => {
-    const configWithSession = {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
-            "X-Session-ID": sessionId
-        }
-    };
-    
-    try {
-        const response = await axios.post(`${BASEURL}${endpoint}`, data, configWithSession);
-        return response;
-    } catch (error) {
-        console.error("POST request error:", error);
-        return error;
-    }
-};
-
-
+// Fonction pour interagir avec le service ChatGPT
 export const askChatGPT = async (payload) => {
-    const config = {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
-        }
-    };
-    
-    try {
-        const response = await axios.post('https://harpie-app.site/api/v1/chat/', payload, config);
-        return response;
-    } catch (error) {
-        console.error("POST request error:", error);
-        return error;
-    }
+    return apiRequest('post', '/chat/', payload);
 };
